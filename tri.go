1357978131017dc9bddec87c13861032eb131fb1
shina <shina.blueprint@gmail.com>
2024-03-05 19:02:22 +0900

    Add conditional execution functions

--- tri.go
+++ tri.go
@@ -17,6 +17,15 @@ func IfExec[T any](condition bool, t func() T, f func() T) T {
 	return f()
 }
 
+// IfExecWithErr executes `t` and returns its result and error if the `condition` is true,
+// otherwise it executes `f` and returns its result and error.
+func IfExecWithErr[T any](condition bool, t func() (T, error), f func() (T, error)) (T, error) {
+	if condition {
+		return t()
+	}
+	return f()
+}
+
 // IfExecOrVal executes a function or returns a value based on a condition.
 // If the condition is true, it executes the provided function `t` and returns its result.
 // If the condition is false, it returns the provided value `f`.
@@ -27,6 +36,15 @@ func IfExecOrVal[T any](condition bool, t func() T, f T) T {
 	return f
 }
 
+// IfExecOrValWithErr executes `t` and returns its result and a nil error if the `condition` is true,
+// otherwise it returns the provided value `f` and a nil error.
+func IfExecOrValWithErr[T any](condition bool, t func() (T, error), f T) (T, error) {
+	if condition {
+		return t()
+	}
+	return f, nil
+}
+
 // IfValOrExec evaluates a condition and returns a value based on the result.
 // If the condition is true, it returns the provided value 't'.
 // If the condition is false, it executes the provided function 'f' and returns its result.
@@ -34,6 +52,15 @@ func IfExecOrVal[T any](condition bool, t func() T, f T) T {
 func IfValOrExec[T any](condition bool, t T, f func() T) T {
 	if condition {
 		return t
+	}
+	return f()
+}
+
+// IfValOrExecWithErr returns the provided value `t` and a nil error if the `condition` is true,
+// otherwise it executes `f` and returns its result and error.
+func IfValOrExecWithErr[T any](condition bool, t T, f func() (T, error)) (T, error) {
+	if condition {
+		return t, nil
 	}
 	return f()
 }
--- tri_test.go
+++ tri_test.go
@@ -1,6 +1,9 @@
 package tri
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestIf(t *testing.T) {
 	result := If(true, "true value", "false value")
@@ -26,6 +29,31 @@ func TestIfExec(t *testing.T) {
 	}
 }
 
+func TestIfExecWithErr(t *testing.T) {
+	result, err := IfExecWithErr(true, func() (string, error) { return "true func", nil }, func() (string, error) { return "false func", nil })
+	if err != nil || result != "true func" {
+		t.Errorf("Expected 'true func', got %s", result)
+	}
+
+	result, err = IfExecWithErr(false, func() (string, error) { return "true func", nil }, func() (string, error) { return "false func", nil })
+	if err != nil || result != "false func" {
+		t.Errorf("Expected 'false func', got %s", result)
+	}
+}
+
+func TestIfExecWithErrReturnsErr(t *testing.T) {
+	expectedErr := errors.New("error")
+	_, err := IfExecWithErr(true, func() (string, error) { return "", expectedErr }, func() (string, error) { return "", nil })
+	if err != expectedErr {
+		t.Errorf("Expected error, got %v", err)
+	}
+
+	_, err = IfExecWithErr(false, func() (string, error) { return "", nil }, func() (string, error) { return "", expectedErr })
+	if err != expectedErr {
+		t.Errorf("Expected error, got %v", err)
+	}
+}
+
 func TestIfExecOrVal(t *testing.T) {
 	result := IfExecOrVal(true, func() string { return "true func" }, "false value")
 	if result != "true func" {
@@ -38,6 +66,31 @@ func TestIfExecOrVal(t *testing.T) {
 	}
 }
 
+func TestIfExecOrValWithErr(t *testing.T) {
+	result, err := IfExecOrValWithErr(true, func() (string, error) { return "true func", nil }, "false value")
+	if err != nil || result != "true func" {
+		t.Errorf("Expected 'true func', got %s", result)
+	}
+
+	result, err = IfExecOrValWithErr(false, func() (string, error) { return "true func", nil }, "false value")
+	if err != nil || result != "false value" {
+		t.Errorf("Expected 'false value', got %s", result)
+	}
+}
+
+func TestIfExecOrValWithErrReturnsErr(t *testing.T) {
+	expectedErr := errors.New("error")
+	_, err := IfExecOrValWithErr(true, func() (string, error) { return "", expectedErr }, "false value")
+	if err != expectedErr {
+		t.Errorf("Expected error, got %v", err)
+	}
+
+	_, err = IfExecOrValWithErr(false, func() (string, error) { return "", nil }, "false value")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
 func TestIfValOrExec(t *testing.T) {
 	result := IfValOrExec(true, "true value", func() string { return "false func" })
 	if result != "true value" {
@@ -47,5 +100,30 @@ func TestIfValOrExec(t *testing.T) {
 	result = IfValOrExec(false, "true value", func() string { return "false func" })
 	if result != "false func" {
 		t.Errorf("Expected 'false func', got %s", result)
+	}
+}
+
+func TestIfValOrExecWithErr(t *testing.T) {
+	result, err := IfValOrExecWithErr(true, "true value", func() (string, error) { return "false func", nil })
+	if err != nil || result != "true value" {
+		t.Errorf("Expected 'true value', got %s", result)
+	}
+
+	result, err = IfValOrExecWithErr(false, "true value", func() (string, error) { return "false func", nil })
+	if err != nil || result != "false func" {
+		t.Errorf("Expected 'false func', got %s", result)
+	}
+}
+
+func TestIfValOrExecWithErrReturnsErr(t *testing.T) {
+	expectedErr := errors.New("error")
+	_, err := IfValOrExecWithErr(true, "true value", func() (string, error) { return "", expectedErr })
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	_, err = IfValOrExecWithErr(false, "true value", func() (string, error) { return "", expectedErr })
+	if err != expectedErr {
+		t.Errorf("Expected error, got %v", err)
 	}
 }
